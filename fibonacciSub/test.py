537371d06457222c413bc34c1b09c0c856002a73
Ayoub <ayoub.alkendi2@mail.dcu.ie>
2025-03-02 02:04:54 +0000

    Added missing functions to the lists

--- fibonacciSub/test.py
+++ fibonacciSub/test.py
@@ -4,8 +4,11 @@ class Test_Fibonacci_Sub(unittest.TestCase):
     
     def test_true(self):
         self.assertEqual(lenLongestFibSubseq([1,2,3,4,5,6,7,8]), 5)
-        self.assertEqual([1,3,7,11,12,14,18], 3)
-        self.assertEqual([1, 4, 6], 0)
+        self.assertEqual(lenLongestFibSubseq([1,3,7,11,12,14,18], 3))
+        self.assertEqual(lenLongestFibSubseq([1, 4, 6], 0))
     
     def test_false(self):
         self.assertFalse(lenLongestFibSubseq([1, 2, 4, 5, 6, 7, 8, 9]), 2)
+
+if __name__ == "__main__":
+    unittest.main()
