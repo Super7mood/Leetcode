6c6c734e88ac605449dd166792c010302acbaf29
Ayoub <ayoub.alkendi2@mail.dcu.ie>
2025-03-02 01:41:13 +0000

    Added test cases

---
+++ fibonacciSub/test.py
@@ -0,0 +1,11 @@
+import unittest
+from fibonacciSub import lenLongestFibSubseq
+class Test_Fibonacci_Sub(unittest.TestCase):
+    
+    def test_true(self):
+        self.assertEqual(lenLongestFibSubseq([1,2,3,4,5,6,7,8]), 5)
+        self.assertEqual([1,3,7,11,12,14,18], 3)
+        self.assertEqual([1, 4, 6], 0)
+    
+    def test_false(self):
+        self.assertFalse(lenLongestFibSubseq([1, 2, 4, 5, 6, 7, 8, 9]), 2)
