cf356a484e6f85aff71c6c2104bae9b9e3fe5f88
Ayoub <ayoub.alkendi2@mail.dcu.ie>
2025-03-02 02:06:22 +0000

    Implementation of tracking previous, current and numbers in the sequence

--- fibonacciSub/fibonacciSub.py
+++ fibonacciSub/fibonacciSub.py
@@ -1,21 +1,33 @@
 def lenLongestFibSubseq(arr):
-        
-        for i in range(len(arr)):
-            fibTotal = arr[i] # contains the number in fibonacci sequence
-            fibList = [arr[i]] # Contains the number in the list
-            fibNum = 0 # The amount of number in a sequence
+    prev = arr[0] # The first number in the sequence
+    curr = arr[1] # The second number in the sequence
+    seqNo = 0 # Stores how many numbers in the fibonacci sequence
+    skippedNums = []
+
+    # Iterate throgh the array
+    for i in range(2, len(arr)):
+
+        total = prev + curr
+        # Checks if the sum of the two number is equal to the next number in the list
+        if total == arr[i]: 
+            prev = curr # Assign the previous number to the current number
+            curr = arr[i] # Assign the current number to the new number found in the sequence
             
-            for j in (i + 1 , len(arr) - 1):
-                fibTotal += arr[j]
-                if fibTotal in arr:
-                    fibList.append(arr[j])
-                    fibList.append(fibTotal)
-                    fibNum += 2
+            # Checks if the sequence started or not and add number accordengly
+            if seqNo > 0:
+                seqNo += 1 # Increment the length of the fibonacci sequence
+            else:
+                seqNo = 3 # Assign the innitial fibonacci sequence numbers to 3 since the fibonacci sequence needs atleast three numbers
         
+        elif total < arr[i]:
+            prev = curr
+            curr = arr[i]
+        
+        else:
+            continue
 
-        print(fibList)
-        return fibNum
 
+    return seqNo
 
 if __name__ == "__main__":
-    lenLongestFibSubseq([int(n) for n in input("Write positive accending numbers from atleast 1: ").split()])
+    print(lenLongestFibSubseq([int(n) for n in input("Write positive accending numbers from atleast 1: ").split()]))
