47d8e4255449d1330c2956f5bcc6dc1c90efd58c
Ayoub <ayoub.alkendi2@mail.dcu.ie>
2025-03-02 03:07:28 +0000

    Added more test casses that failed in leetcode

--- fibonacciSub/test.py
+++ fibonacciSub/test.py
@@ -6,6 +6,9 @@ class Test_Fibonacci_Sub(unittest.TestCase):
         self.assertEqual(lenLongestFibSubseq([1,2,3,4,5,6,7,8]), 5)
         self.assertEqual(lenLongestFibSubseq([1,3,7,11,12,14,18]), 3)
         self.assertEqual(lenLongestFibSubseq([1, 4, 6]), 0)
+        self.assertEqual(lenLongestFibSubseq([1, 2, 5, 6, 9]), 3)
+        self.assertEqual(lenLongestFibSubseq([1,3,7,11,12,14,18]), 3)
+        self.assertEqual(lenLongestFibSubseq([2,4,7,8,9,10,14,15,18,23,32,50]), 5)
     
     def test_false(self):
         self.assertNotEqual(lenLongestFibSubseq([1, 2, 4, 5, 6, 7, 8, 9]), 2)
