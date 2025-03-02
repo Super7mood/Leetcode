4de2d15119e4cf6fc4d420b361f18ef281806a75
Ayoub <ayoub.alkendi2@mail.dcu.ie>
2025-03-02 03:07:53 +0000

    Added implementation that records the maximum sequence number

--- fibonacciSub/fibonacciSub.py
+++ fibonacciSub/fibonacciSub.py
@@ -1,18 +1,40 @@
 def lenLongestFibSubseq(arr):
     prev = arr[0] # The first number in the sequence
+    prevIndex = 0 # Stores the position of the previous number
+
     curr = arr[1] # The second number in the sequence
+    currIndex = 1 # Stores the position of the current number
+
     seqNo = 0 # Stores how many numbers in the fibonacci sequence
-    skippedNums = []
+    tempSeqNo = 0
+    combinationQueue = []
 
     # Iterate throgh the array
-    for i in range(2, len(arr)):
+    i = 2
+    while i < len(arr) + 1:
 
-        total = prev + curr
+        # Checks if there is still combinations of the sequence we didn't iterate and check
+        if i == len(arr) and len(combinationQueue) > 0:
+            prevIndex, currIndex = combinationQueue.pop(0) # Assign the indexes
+            i = currIndex + 1 if currIndex != len(arr) - 1 else currIndex
+            prev = arr[prevIndex] 
+            curr = arr[currIndex]
+            if seqNo > tempSeqNo:
+                tempSeqNo = seqNo
+            seqNo = 0
+        
+        elif i == len(arr):
+            break
+
+        total = prev + curr # Get the sum of the numbers
+
         # Checks if the sum of the two number is equal to the next number in the list
         if total == arr[i]: 
             prev = curr # Assign the previous number to the current number
+            prevIndex = currIndex
             curr = arr[i] # Assign the current number to the new number found in the sequence
-            
+            currIndex = i
+
             # Checks if the sequence started or not and add number accordengly
             if seqNo > 0:
                 seqNo += 1 # Increment the length of the fibonacci sequence
@@ -20,13 +42,19 @@ def lenLongestFibSubseq(arr):
                 seqNo = 3 # Assign the innitial fibonacci sequence numbers to 3 since the fibonacci sequence needs atleast three numbers
         
         elif total < arr[i]:
+            combinationQueue.append((prevIndex, i))
             prev = curr
+            prevIndex = currIndex
             curr = arr[i]
+            currIndex = i
         
-        else:
-            continue
+        if seqNo > tempSeqNo:
+            tempSeqNo = seqNo
+        
+        i += 1
 
 
+    seqNo = tempSeqNo
     return seqNo
 
 if __name__ == "__main__":
